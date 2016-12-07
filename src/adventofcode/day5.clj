4001e642b51ae58d66cb93a2fb7db211c78bf89a
sooheon <tngjs0@gmail.com>
2016-12-08 06:10:42 +0900

    Day 5: much cleaner solution with threading macro

--- src/adventofcode/day5.clj
+++ src/adventofcode/day5.clj
@@ -7,30 +7,29 @@
 
 (def door-id "ffykfhsq")
 
-(loop [n 0
-       remaining 8
-       acc ""]
-  (let [md5-sum (md5 (str "ffykfhsq" n))]
-    (if (= 0 remaining)
-      acc
-      (if (.startsWith md5-sum "00000")
-        (do (print md5-sum (nth md5-sum 5) "\n")
-            (recur (inc n) (dec remaining) (str acc (nth md5-sum 5))))
-        (recur (inc n) remaining acc)))))
+(->> (range)
+     (map #(md5 (str "ffykfhsq" %)))
+     (filter #(.startsWith % "00000"))
+     (map #(nth % 5))
+     (take 8)
+     (apply str))
 
 ;; part 2
 
 (loop [n 0
        remaining 8
-       acc [nil nil nil nil nil nil nil nil]]
+       pass [nil nil nil nil nil nil nil nil]]
   (let [md5-sum (md5 (str "ffykfhsq" n))]
     (if (= 0 remaining)
-      (str/join acc)
+      ;; when no more remaining, return the result
+      (str/join pass)
+
+      ;; if spots still remain to be filled..
       (if (.startsWith md5-sum "00000")
         (let [pos (- (int (nth md5-sum 5)) (int \0))
               passchar (nth md5-sum 6)]
-          (if (and (>= 7 pos) (nil? (get acc pos)))
+          (if (and (>= 7 pos) (nil? (get pass pos)))
             (do (print md5-sum pos passchar "\n")
-                (recur (inc n) (dec remaining) (assoc acc pos passchar)))
-            (recur (inc n) remaining acc)))
-        (recur (inc n) remaining acc)))))
+                (recur (inc n) (dec remaining) (assoc pass pos passchar)))
+            (recur (inc n) remaining pass)))
+        (recur (inc n) remaining pass)))))
